aa0359715f416737c59f55d7e16f541c953ef654
LourdesBS7 <LourdesBS7(GitHub)>
2025-06-12 03:34:00 -0400

    ya render

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
-app.use(cors({
+/* app.use(cors({
     origin: 'http://localhost:3000', // Permite CORS desde el mismo puerto
     credentials: true
+})); */
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://sistemafacturacionelectronica.onrender.com'],
+    credentials: true
 }));
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'frontend'))); // Servir archivos estáticos
 
