0eb6195713a1ba988d7411d9964da1989906c6ef
LourdesBS7 <LourdesBS7(GitHub)>
2025-06-12 03:38:56 -0400

    login

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         try {
-            const response = await fetch('http://localhost:3000/api/usuarios/login', {
+            const response = await fetch('https://sistemafacturacion.onrender.com/api/usuarios/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
